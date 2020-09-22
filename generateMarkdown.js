5475f136ca4e876db2a1bbb318833dd14ece593d
Steve Roday <srrodrig.rodriguez@gmail.com>
2020-09-22 14:07:38 -0500

    Added generateMarkdown function and question bank

---
+++ generateMarkdown.js
@@ -0,0 +1,43 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  ${data.questions}
+
+`;
+}
+
+module.exports = generateMarkdown;
---
+++ index.js
@@ -0,0 +1,77 @@
+var fs = require ("fs");
+var inquirer = require ("inquirer");
+var path = require ("path");
+const generate = require ("./generateMarkdown");  
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// array of questions for user
+
+    function init() {
+        return inquirer.prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project's title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project.",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How do you install your program?",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How do you use your program?",
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Select a license type:",
+        Choices: "MIT License \n GNU GPLv3 \n GNU AGPLv3 \n Mozilla Public License 2.0 \n Apache License 2.0 \n Boost Software License \n The Unlicense",
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "How can people contribute to your project?",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Describe any accompanying tests you have programmed and how to run them. ",
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "How can you be reached with questions?",
+    }
+]);
+};
+
+// function call to initialize program
+init()
+// function to write README file
+    .then(function(data) {
+        generate(data);
+
+        return writeFileAsync("readMe.md", md);
+    })
+    .then(function() {
+        console.log("Successfully wrote to readMe.md");
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
+
+
+// function to initialize program
+//function init() {
+
+//}
+
+
